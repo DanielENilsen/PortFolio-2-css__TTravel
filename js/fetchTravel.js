106510b8ee6e301a537234b82e6bdd0253f80c19
DanielENilsen <danieleidenilsen@hotmail.com>
2020-08-20 20:24:39 +0200

    update

---
+++ js/fetchTravel.js
@@ -0,0 +1,30 @@
+
+
+var travelPlaces = "../json/travelPlaces.json";
+var travelGrid = document.getElementById('travelGrid');
+
+fetch(travelPlaces).then(reponsive => reponsive.json()).then(travelApi => { 
+    console.log(travelApi);
+    travelFunction(travelApi);
+
+}).catch(Error => console.log(Error));
+
+
+var travelFunction = function (reponsive) { 
+    for(let i = 0 ; i < reponsive.length;i++) {
+        travelGrid.innerHTML += `<div class = "col-lg-10 col-xl-4">
+                                    <div class = "card">
+                                        <div class = "card__edith">
+                                            <div class = "img-square-wrapper">
+                                                <img class = "card-img-top" src = "${reponsive[i].img}>"
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <h4 class="card-title">Card title</h4>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                    </div>`;
+                                
+    }    
+}
